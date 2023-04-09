8630c878203d994b70ccec222fc21c8b1db4cc09
Eliiisa <elisaklein66@gmail.com>
2023-04-09 16:26:34 +0200

    migrate to Symfony 6.2

---
+++ src/Entity/Task.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ORM\Table("tasks")]
+#[ORM\HasLifecycleCallbacks]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(options: ["default" => "CURRENT_TIMESTAMP"])]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Vous devez saisir un titre.")]
+    private string $title;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: "Vous devez saisir du contenu.")]
+    private string $content;
+
+    #[ORM\Column(options: ["default" => false])]
+    private bool $isDone = false;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->isDone = false;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function isDone(): bool
+    {
+        return $this->isDone;
+    }
+
+    public function toggle($flag): void
+    {
+        $this->isDone = $flag;
+    }
+}
